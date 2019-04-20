7b358d6caacb0b7fd16cecf025cc6b806606fcd7
Cedric Orejola <cedricorejola@gmail.com>
2019-04-20 15:49:02 -0700

    progress save

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@
 //     3.1 var gifArray =[]; user input .val() to be .push()'d to array
 //     3.2 for loop gifArray to create buttons
 //     3.3 buttons .appended to top of the HTML (based on the HTML Layout)
-//     3.4 on submit, clear input field
+//     3.4 on submit, clear input field !need some assistance on this
 
 // 4.0 button functionality
 //     4.1 button click .empty()'s html gif display area 
@@ -31,42 +31,50 @@
 
 var apiKey = "Ul7ntbONsk4eCTISBH1xjVns9Ng61MN9";
 var gifArray = [];
-var queryURL =  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    ____ + "&api_key=dc6zaTOxFJmzC&limit=10";
+
 
 $('#addGif').on("click", function (event) {
     event.preventDefault();
-
-    console.log(this)
+    // console.log(this)
 
     var input = $('#gifInput').val().trim();
-    console.log(input)
+    // console.log(input)
 
     gifArray.push(input);
-    console.log(gifArray)
+    // console.log(gifArray)
 
     generateButtons();
 
     $('#gifInput').empty();
-
+    // $('#gifForm').reset(); // follow up question to phil
 });
 
 function generateButtons() {
     $('#gifButtons').empty();
 
+
     for (var i = 0; i < gifArray.length; i++) {
         var button = $('<button>').text(gifArray[i]).attr("data-gif", gifArray[i]);
+        button.attr("class", "gif");
         $('#gifButtons').append(button);
     }
-    $('#gifInput').empty();
-}
-
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response)
 
 }
+// button makes ajax call
+//use document.on to call on elements created
+$(document).on("click", " .gif", function () {
+
+    var input = $(this).attr("data-gif")
+    console.log(input)
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        input + "&api_key=" + apiKey + "&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    })
+
+})
+
